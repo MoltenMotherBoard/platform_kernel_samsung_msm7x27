4eeb43bdc6f96b62a0bfe68fd060dff52ab6650b
Kees Cook <kees.cook@canonical.com>
2011-05-04 08:38:56 -0700

    [CPUFREQ] remove redundant sprintf from request_module call.
    Since format string handling is part of request_module, there is no
    need to construct the module name. As such, drop the redundant sprintf
    and heap usage.
    
    Signed-off-by: Kees Cook <kees.cook@canonical.com>
    Signed-off-by: Dave Jones <davej@redhat.com>
    
    Conflicts:
    
    	drivers/cpufreq/cpufreq.c
    
    Conflicts:
    
    	drivers/cpufreq/cpufreq.c

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -415,18 +415,12 @@ static int cpufreq_parse_governor(char *str_governor, unsigned int *policy,
 			char *name = kasprintf(GFP_KERNEL, "cpufreq_%s",
 								str_governor);
 
-			if (name) {
-				int ret;
+			mutex_unlock(&cpufreq_governor_mutex);
+			ret = request_module("cpufreq_%s", str_governor);
+			mutex_lock(&cpufreq_governor_mutex);
 
-				mutex_unlock(&cpufreq_governor_mutex);
-				ret = request_module("%s", name);
-				mutex_lock(&cpufreq_governor_mutex);
-
-				if (ret == 0)
-					t = __find_governor(str_governor);
-			}
-
-			kfree(name);
+			if (ret == 0)
+				t = __find_governor(str_governor);
 		}
 
 		if (t != NULL) {
