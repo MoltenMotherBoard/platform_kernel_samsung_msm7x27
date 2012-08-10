e63042e29e409c3ba597aad4fc88c350961ce2e9
Dāvis Mālnieks <daavis.90@gmail.com>
2012-08-10 12:27:04 +0300

    properly fix USB

--- include/linux/usb/composite.h
+++ include/linux/usb/composite.h
@@ -355,17 +355,10 @@ struct usb_composite_dev {
 	/* protects at least deactivation count */
 	spinlock_t			lock;
 
-	/* switch indicating connected/disconnected state */
-	struct switch_dev		sw_connected;
-	/* switch indicating current configuration */
-	struct switch_dev		sw_config;
-	/* current connected state for sw_connected */
-	bool				connected;
-        /* used by usb_composite_force_reset to avoid signalling switch changes */
-        bool                            mute_switch;
-        struct work_struct switch_work;
-
-	struct switch_dev		sdev;
+	struct switch_dev sdev;
+	/* used by usb_composite_force_reset to avoid signalling switch changes */
+	bool				mute_switch;
+	struct work_struct switch_work;
 };
 
 extern int usb_string_id(struct usb_composite_dev *c);
